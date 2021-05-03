03cb008c99a1e0c132a81e647b11f4be9ada58e9
Helio Schultz <helioschultz_1@msn.com>
2021-05-03 21:03:50 +0100

    Created a env-example file with the database credentials placeholders

---
+++ env-example.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    "dbconnection" => [
+        "host" => "your api address",
+        "db" => "your database name",
+        "username" => "username",
+        "password" => "password"
+    ],
+    "environment" => "dev" 
+];
